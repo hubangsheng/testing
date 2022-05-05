efb31afee056bb8c79440eb6926adc0bb956e844
hubangsheng <hubang.good@163.com>
2022-05-05 14:10:43 +0800

    添加web自动搜索“霍格沃兹测试开发”演练代码

--- AllureDemo/webautotest/webtest.py
+++ AllureDemo/webautotest/webtest.py
@@ -1,4 +1,30 @@
 from selenium import webdriver
-driver = webdriver.Chrome()
-driver.get("https://www.baidu.com")
-driver.quit()
+# driver = webdriver.Chrome()
+# driver.get("https://www.baidu.com")
+# driver.quit()
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+
+class TestDemo():
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
+
+    def teardown_method(self, method):
+        self.driver.quit()
+
+    def test_sogou(self):
+        # 打开网页，设置窗口
+        self.driver.get("https://www.sogou.com")
+        self.driver.set_window_size(1233, 693)
+        self.driver.find_element(By.ID, "query").click()
+        self.driver.find_element(By.ID, "query").send_keys("霍格沃兹测试开发")
+        # 点击搜索
+        self.driver.find_element(By.ID, "stb").click()
+        element = self.driver.find_element(By.ID,"stb")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+
+        res_element = self.driver.find_element(By.CSS_SELECTOR,"#sogou_vr_30000000_0 > em")
+        assert res_element.text == "霍格沃兹测试开发"
