1743a396f3372b19c8abbacd24c4103a744a9a60
hubangsheng <hubang.good@163.com>
2022-05-23 17:21:15 +0800

    将异常写在装饰器中，然后通过调用装饰器对需要的用例进行加载

--- webTestDemo/webautotest/data_record_exception/test_baidu.py
+++ webTestDemo/webautotest/data_record_exception/test_baidu.py
@@ -4,20 +4,40 @@ import allure
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+'''
+目标1、实现代码异常的时候，截图/打印page_source
+实现方法：try catch 配合截图/ page_source操作
 
-class TestBaidu:
-    def test_baidu(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get("https://www.baidu.com")
+问题一、异常处理会影响用例本身的结果
+解决方案：在exception 之后再把异常抛出
+
+问题二、异常捕获处理代码和实际的业务无关，不能耦合
+解决方案：使用装饰器装饰用例或者相关方法，就不会体现在源码中了，（注意：一定要先学习装饰器）
+    1、先把装饰器的架子搭好
+    2、把相关的逻辑嵌套进来
+'''
+
+
+def ui_exception_record(func):
+    def inner(*args, **kwargs):
+        # 获取被装饰方法的self 也就是实例对象
+        # 通过self就可以拿到声明的实例变量driver
+        # 前提条件：1、被装饰的方法是一个实例方法， 2、实例需要有实例变量self.driver
+
         try:
-            self.driver.find_element(By.ID, "su")
+            # 当被装饰的方法/函数发生异常就捕获并做数据记录
+            return func(*args, **kwargs)
         except Exception:
+            driver = args[0].driver
             timestamp = int(time.time())
+            # 注意：需要提前创建好images 目录
             image_path = f"./images/image_{timestamp}.PNG"
             page_source_path = f"./page_source/page_source_{timestamp}.html"
-            self.driver.save_screenshot(image_path)
+            # 截图
+            driver.save_screenshot(image_path)
+            # 记录page_source
             with open(page_source_path, "w", encoding="u8") as f:
-                f.write(self.driver.page_source)
+                f.write(driver.page_source)
 
             # 将截图放到报告的数据中
             allure.attach.file(image_path, name="pagesource", attachment_type=allure.attachment_type.PNG)
@@ -25,3 +45,24 @@ class TestBaidu:
             # 如果想要 html 源码格式使用text，如果想要页面格式使用html
             allure.attach.file(page_source_path, name="pagesource", attachment_type=allure.attachment_type.HTML)
             raise Exception
+    return inner
+
+
+class TestBaidu:
+    def setup_class(self):
+        self.driver = webdriver.Chrome()
+
+    def teardown_class(self):
+        self.driver.quit()
+
+    @ui_exception_record
+    def findui(self):
+        # print(res)
+        return self.driver.find_element(By.ID, "su")
+
+
+    def test_baidu(self):
+        self.driver.get("https://www.baidu.com/")
+        # print(self.findui)
+        self.findui().click()
+
