49a57c1f7ec1a2d0e9fd567b68073511ac63064d
hubangsheng <hubang.good@163.com>
2022-05-24 16:02:08 +0800

    使用pytest.mark.parametrize 装饰器对用例进行参数化

--- webTestDemo/webautotest/po_test_demo/test_litemall/test_litemall_report_po.py
+++ webTestDemo/webautotest/po_test_demo/test_litemall/test_litemall_report_po.py
@@ -1,6 +1,7 @@
 import time
 
 import allure
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -18,25 +19,29 @@ class TestLitemall:
         self.home.do_quit()
 
     # 新增功能
-    def test_add_type(self):
+    @pytest.mark.parametrize("category_name", ["a", "b", "c"])
+    def test_add_type(self, category_name):
         """系统首页：进入商品类目"""
         """类目列表页：点击添加"""
         """创建类目页面：创建类目"""
         """类目列表页：获取操作结果"""
-        res = self.home\
+        list_page = self.home\
             .go_to_category()\
             .click_add()\
-            .create_category("新增商品测试")\
-            .get_operate_result()
+            .create_category(category_name)
+        res = list_page.get_operate_result()
         assert "创建成功" == res
+        # 数据清理动作，一定要放在断言之后，免得影响测试结果
+        list_page.delete_category(category_name)
 
 #     删除功能
-    def test_delete_type(self):
+    @pytest.mark.parametrize("category_name", ["delA", "delB", "delC"])
+    def test_delete_type(self, category_name):
         res = self.home\
             .go_to_category()\
             .click_add()\
-            .create_category("删除商品测试")\
-            .delete_category("删除商品测试")\
+            .create_category(category_name)\
+            .delete_category(category_name)\
             .get_delete_result()
         assert "删除成功" == res
 
