f3222fd01e2c67dbf57b0b8913e9e2799ab03111
hubangsheng <hubang.good@163.com>
2022-05-27 15:52:51 +0800

    添加cookie复用的代码，以企业微信为例

---
+++ webTestDemo/webautotest/cookies_test_demo/test_cookies.py
@@ -0,0 +1,39 @@
+import time
+import yaml
+from selenium import webdriver
+
+
+class TestWeworkLogin:
+    def setup_class(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+        self.driver.maximize_window()
+
+    def teardown_class(self):
+        self.driver.quit()
+
+    def test_save_cookies(self):
+        # 1、访问登录页面
+        self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx")
+        # 2、手工扫码（直接等待）
+        time.sleep(10)
+        # 3、获取浏览器cookies
+        cookies = self.driver.get_cookies()
+        print(cookies)
+        # 4、保存cookies
+        with open("./data/cookies.yaml", "w") as f:
+            yaml.safe_dump(data=cookies, stream=f)
+
+    def test_get_cookie(self):
+        """植入cookie"""
+        # 1、访问企业微信首页
+        self.driver.get("https://work.weixin.qq.com/wework_admin/frame")
+        # 2、获取本地的cookie
+        with open("./data/cookies.yaml", "r") as f:
+            cookies = yaml.safe_load(f)
+        print(cookies)
+        # 3、植入cookie
+        for ck in cookies:
+            self.driver.add_cookie(ck)
+        # 4、访问企业微信首页
+        self.driver.get("https://work.weixin.qq.com/wework_admin/frame")
